b1be2867bd5f956b11b63b7cbd0b3076935b3409
caiosilvatrofino <caios.trofino@gmail.com>
2025-03-31 21:13:28 -0300

    upd project

--- auto-project/src/app/components/carros/carrosdetail/carrosdetail.component.ts
+++ auto-project/src/app/components/carros/carrosdetail/carrosdetail.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Carro } from '../../../models/carro';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-carrosdetail',
-  imports: [RouterLink],
+  imports: [[FormsModule, RouterLink]],
   templateUrl: './carrosdetail.component.html',
   styleUrl: './carrosdetail.component.scss'
 })
 export class CarrosdetailComponent {
+
+  carro: Carro = new Carro(0,"",0,"","","",0,"",0,"");
+
+  router = inject(ActivatedRoute)
+  router2 = inject(Router);
+
+  constructor(){
+    let id = this.router.snapshot.params['id'];
+    if(id > 0) {
+      this.findById(id);
+    }
+  }
+
+  findById(id: number){
+    let carroReturned: Carro = new Carro(id, 'Punto', 2014, 'Automatico','Fiat','554-151', 130.000, 'FLEX', 35.000, 'SIM');
+    this.carro = carroReturned;
+  }
+
+  save(){
+    if(this.carro.id > 0 ) {
+      alert('Editado com sucesso com sucesso!');
+      this.router2.navigate(['admin/carros'], {state: {carroEdited: this.carro}});
+    }else {
+      alert('Salvo com sucesso!');
+      this.router2.navigate(['admin/carros'], {state: {carroNovo: this.carro}});
+    }
+    
+  }
 
 }
--- auto-project/src/app/components/carros/carroslist/carroslist.component.ts
+++ auto-project/src/app/components/carros/carroslist/carroslist.component.ts
@@ -16,7 +16,26 @@ export class CarroslistComponent {
     this.lista.push(new Carro(1, 'Fiesta', 2009, 'Manual','Ford','324-41241',134.000 , 'GASOLINA', 33.000, 'SIM'));
     this.lista.push(new Carro(2, 'Ford ka', 2008, 'Manual', 'Ford','324-141', 245.000, 'FLEX', 20.000, 'NÃO'))
     this.lista.push(new Carro(3, 'Punto', 2014, 'Automatico','Fiat','554-151', 130.000, 'FLEX', 35.000, 'SIM'))
+    this.lista.push(new Carro(4, 'Ford ka', 2008, 'Manual', 'Ford','324-141', 245.000, 'FLEX', 20.000, 'NÃO'))
+    this.lista.push(new Carro(5, 'Punto', 2014, 'Automatico','Fiat','554-151', 130.000, 'FLEX', 35.000, 'SIM'))
+    this.lista.push(new Carro(6, 'Ford ka', 2008, 'Manual', 'Ford','324-141', 245.000, 'FLEX', 20.000, 'NÃO'))
+    this.lista.push(new Carro(7, 'Punto', 2014, 'Automatico','Fiat','554-151', 130.000, 'FLEX', 35.000, 'SIM'))
 
+    let carroNovo = history.state.carroNovo;
+    let carroEdited = history.state.carroEdited;
+    
+    if(carroNovo){
+      let tamanho = this.lista.length;
+      carroNovo.id = tamanho + 1;
+      this.lista.push(carroNovo);
+      
+    }
+
+    if(carroEdited){
+      let indice = this.lista.findIndex(x => {return x.id == carroEdited.id});
+      this.lista[indice] = carroEdited;
+
+    }
   }
 
   deleteById(carro: Carro){
